2136449e9eb48b735cd63c51aeaa61e93d68c90f
Pedro Sandoval <psandoval2015@gmail.com>
2016-03-17 17:14:55 -0700

    Done.

--- javacs-lab01/src/com/flatironschool/javacs/ListClientExample.java
+++ javacs-lab01/src/com/flatironschool/javacs/ListClientExample.java
@@ -2,16 +2,17 @@ package com.flatironschool.javacs;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ListClientExample {
 	private List list;
-	
+
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new ArrayList();
 	}
 
 	public List getList() {
-		return list;		
+		return list;
 	}
 
 	public static void main(String[] args) {
